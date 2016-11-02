b9514cce2032dc7cef35883cf1a432706c066cfb
zsofinnemeth <zsofinnemeth@gmail.com>
2016-11-02 20:25:47 +0100

    workshop05 corrected

--- week-03/day-1/idea/src/Workshop05.java
+++ week-03/day-1/idea/src/Workshop05.java
@@ -4,9 +4,9 @@
 public class Workshop05 {
     public static void main(String[] args) {
         int e = 8;
-         e= e*e;
+
         // please cube of e's value
 
-        System.out.println(e);
+        System.out.println(Math.cbrt(e));
     }
 }
