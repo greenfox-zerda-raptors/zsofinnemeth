d6e8cc99ad4c53d93ca70d4520f81746a498876b
zsofinnemeth <zsofinnemeth@gmail.com>
2016-11-04 16:52:13 +0100

    workshop 10 upload

--- week-03/day-3/src/Workshop10.java
+++ week-03/day-3/src/Workshop10.java
@@ -7,9 +7,17 @@ public class Workshop10{
         // in colors[0] store the shades of green: "lime", "forest green", "olive", "pale green", "spring green"
         // in colors[1] store the shades of red: "orange red", "red", "tomato"
         // in colors[2] store the shades of pink: "orchid", "violet", "pink", "hot pink"
-        String[][] colors = new String[3][];
+        String[][] colors = {
+                { "lime", "forest green", "olive", "pale green", "spring green" },
+                { "orange red", "red", "tomato" },
+                { "orchid", "violet", "pink", "hot pink" } };
 
-        // Task 2: Print the values of the array to the console
-      
+        for (int i = 0; i < colors.length; i++) {
+            System.out.print(colors[i][0]+ " " );
+           for (int j = 1; j < colors[i].length; j++) {
+             System.out.print(colors[i][j] + " ");
+           }
+            System.out.println();
+        }
     }
 }
