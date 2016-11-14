8727e45cc8bc4427237ab49121ce3ad9ee00cab0
zsofinnemeth <zsofinnemeth@gmail.com>
2016-11-14 15:27:17 +0100

    workshops 01-06

--- week-05/day-1/src/Car.java
+++ week-05/day-1/src/Car.java
@@ -12,23 +12,40 @@ public class Car {
         this.color = color;
         this.kmOdometer = kmOdometer;
         this.typeCar = typeCar;
-
     }
+    public Car(String typeCar, String color) {
+        this.sizeEngine = 1600;
+        this.color = color;
+        this.kmOdometer = 15;
+        this.typeCar = typeCar;
+    }
+    public Car(int sizeEngine, int kmOdometer) {
+        this.sizeEngine = sizeEngine;
+        this.color = "red";
+        this.kmOdometer = kmOdometer;
+        this.typeCar = "Mazda";
+    }
+
+
 
     public void drive(int clicks) {
         kmOdometer += clicks;
 
-        System.out.printf("The %s %s odometer now shows %d\n", color, typeCar, kmOdometer);
+
         if (clicks < 20) {
-            System.out.printf("brrm, %s just drove around town %d clicks.\n", typeCar, clicks);
-        } if (clicks < 50) {
-            System.out.printf("brrm, screech, brrm, screech; %s commuted %d clicks.\n", typeCar, clicks);
+            System.out.printf("Brrm, the %s %s just drove around town %d clicks.\n", color, typeCar, clicks);
+        } else if (clicks < 50) {
+            System.out.printf("Brrm, screech, brrm, screech; the %s %s commuted %d clicks.\n", color, typeCar, clicks);
         } else {
-            System.out.printf("whee, %s %d clicks.\n", typeCar, clicks);
+            System.out.printf("Whee, the %s %s %d clicks.\n", color, typeCar, clicks);
         }
+        System.out.printf("The %s %s odometer now shows %d.\n", color, typeCar, kmOdometer);
 
     }
 
+     public String toString() {
+     return "This " + typeCar + " is " + color + ", has " + sizeEngine + " cc engine and clocked " + kmOdometer + " km's.";
+    }
 
     public int getSizeEngine() {
         return sizeEngine;
--- week-05/day-1/src/Workshop01.java
+++ week-05/day-1/src/Workshop01.java
@@ -6,11 +6,23 @@ public class Workshop01 {
         Car myCar1 = new Car("Mazda", "red", 1600, 12312);
         Car myCar2 = new Car("Ford", "silver", 2500, 152312);
         Car myCar3 = new Car("Beamer", "green", 2800, 42521);
-
-        myCar1.drive(15);
+        Car myCar5 = new Car("Opel", "white", 1400, 25);
+        Car myCar6 = new Car("VW", "grey");
+        Car myCar7 = new Car(1100, 15);
 
         System.out.printf("This %s is %s, has %d cc engine and clocked %d km's\n", myCar1.getTypeCar(), myCar1.getColor(), myCar1.getSizeEngine(),myCar1.getKmOdometer());
         System.out.printf("This %s is %s, has %d cc engine and clocked %d km's\n", myCar2.getTypeCar(), myCar2.getColor(), myCar2.getSizeEngine(),myCar2.getKmOdometer());
         System.out.printf("This %s is %s, has %d cc engine and clocked %d km's\n", myCar3.getTypeCar(), myCar3.getColor(), myCar3.getSizeEngine(),myCar3.getKmOdometer());
+
+        System.out.println(myCar1.toString());
+        System.out.println(myCar2.toString());
+        System.out.println(myCar3.toString());
+        System.out.println(myCar5.toString());
+        System.out.println(myCar6.toString());
+        System.out.println(myCar7.toString());
+
+        myCar1.drive(15);
+        myCar2.drive(45);
+        myCar3.drive(60);
     }
 }
