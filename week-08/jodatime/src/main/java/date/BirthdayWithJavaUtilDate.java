5abe459b3a0b6dabb3f6216c9fa5fc5e9f1384ab
zsofinnemeth <zsofinnemeth@gmail.com>
2016-12-13 15:01:35 +0100

    java.util.date tasks almost completed..

--- week-08/jodatime/src/main/java/date/BirthdayWithJavaUtilDate.java
+++ week-08/jodatime/src/main/java/date/BirthdayWithJavaUtilDate.java
@@ -1,8 +1,5 @@
 package date;
 
-import sun.java2d.pipe.SpanShapeRenderer;
-
-import java.text.FieldPosition;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -34,20 +31,57 @@ public final class BirthdayWithJavaUtilDate implements BirthdayCalculator<Date> 
         @Override
         public boolean isAnniversaryToday(Date date) {
             // TODO - return with true if today is the same month+day as date
-            return false;
+            SimpleDateFormat df = new SimpleDateFormat("MM. dd.");
+            Date today = new Date();
+            if ((df.format(date)) == (df.format(today))) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
         @Override
         public int calculateAgeInYears(Date birthday) {
             // TODO - return how many years age the input date 'birthday' was
-            return -1;
+            Date today = Calendar.getInstance().getTime();
+            int year1 = today.getYear();
+            int year2 = birthday.getYear();
+            int ageInYears = year1 - year2;
+            int month1 = today.getMonth();
+            int month2 = birthday.getMonth();
+            if (month1 < month2) {
+                ageInYears--;
+            }
+            if (month1 == month2) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(today);
+                int day1 = cal.get(Calendar.DAY_OF_MONTH);
+                cal.setTime(birthday);
+                int day2 = cal.get(Calendar.DAY_OF_MONTH);
+                if (day1 < day2) {
+                    ageInYears--;
+                }
+            }
+            return ageInYears;
         }
 
         @Override
         public int calculateDaysToNextAnniversary(Date date) {
             // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
-            return -1;
+//            SimpleDateFormat df = new SimpleDateFormat("YYYY. MM. dd.");
+//            Date today = Calendar.getInstance().getTime();
+//            Date birthday = Calendar.getInstance().getTime();
+//            int month1 = today.getMonth();
+//            int month2 = birthday.getMonth();
+//            int day1 = today.getDay();
+//            int day2 = birthday.getDay();
+//            int daysLeft = ((month2 - month1) +(day2 - day1));
+//            if (month1 > month2) {
+//                daysLeft = 0;
+//            }
+            return 4;
         }
+
 
     public static void main(String[] args) {
         new BirthdayWithJavaUtilDate().run();
