de792099c7f6875131430d0a68ebd50e753b1037
zsofinnemeth <zsofinnemeth@gmail.com>
2016-12-14 13:15:24 +0100

    jdbc workshop, first two tasks completed

---
+++ week-08/ORM/src/Account.java
@@ -0,0 +1,40 @@
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by false on 2016. 12. 14..
+ */
+@DatabaseTable (tableName = "accounts")
+
+public class Account {
+    @DatabaseField(id = true)
+
+    String name;
+    @DatabaseField(canBeNull = false)
+    String password;
+
+    public Account(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public Account() {
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
