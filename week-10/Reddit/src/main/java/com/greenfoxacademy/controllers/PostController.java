1229c2ae80bd58c51d8ee58ddbaf0c464b2846d6
zsofinnemeth <zsofinnemeth@gmail.com>
2017-01-04 16:41:38 +0100

    Reddit app, completed tasks: edit, create new post, back button, SQL functionality

--- week-10/Reddit/src/main/java/com/greenfoxacademy/controllers/PostController.java
+++ week-10/Reddit/src/main/java/com/greenfoxacademy/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.domain.Post;
 import com.greenfoxacademy.domain.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,6 +28,33 @@ public class PostController {
     public ModelAndView delete(@PathVariable long id) {
         repository.delete(id);
         return new ModelAndView("redirect:/posts");
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String newProject() {
+        return "posts/new";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView create(@RequestParam("message") String comment) {
+    repository.save(new Post(comment));
+        return new ModelAndView("redirect:/posts");
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ModelAndView update(@RequestParam("post_id") Long id,
+                               @RequestParam("message") String message) {
+        Post post = repository.findOne(id);
+        post.setMessage(message);
+        repository.save(post);
+        return new ModelAndView("redirect:/posts");
+    }
+
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+    public String edit(@PathVariable long id, Model model) {
+    Post post = repository.findOne(id);
+        model.addAttribute("post", post);
+        return "posts/edit";
     }
 }
 
