39c325895229c0d63b2e01817df7b27b48b81c49
zsofinnemeth <zsofinnemeth@gmail.com>
2017-01-04 13:03:49 +0100

    reddit workshop Hello World or environment finished

---
+++ week-10/Reddit/src/main/java/com/greenfoxacademy/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String helloIndex() {
+        return "Hello World!";
+    }
+}
