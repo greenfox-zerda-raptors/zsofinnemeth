7bdc691dfd0f5f05dd808a7b5992e5bcef9e5721
Andras Nemeth <guni82hu@gmail.com>
2017-01-17 00:14:13 +0100

    my first android app

---
+++ FirstAndroidApp/app/src/main/java/mockmedia/velemjaro/com/myapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package mockmedia.velemjaro.com.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("faaaak");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        TextView newText = (TextView) findViewById(R.id.fvcku);
+        newText.setText(R.string.action_fuk);
+        Button pushTheTempoButton = (Button) findViewById(R.id.button);
+        final EditText textLeft = (EditText) findViewById(R.id.textEditLeft);
+        final EditText textRight = (EditText) findViewById(R.id.textEditRight);
+        final TextView result = (TextView) findViewById(R.id.resultView);
+        pushTheTempoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer left = Integer.valueOf(textLeft.getText().toString());
+                Integer right = Integer.valueOf(textRight.getText().toString());
+                result.setText(String.valueOf(left + right));
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
